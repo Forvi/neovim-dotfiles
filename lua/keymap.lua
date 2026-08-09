c70efb2002ec44b956f8babe7b51419c4eeb80f2
Forvi <nforvi@mail.ru>
2026-08-09 15:20:13 +0500

    changed files open call

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -5,8 +5,10 @@ vim.keymap.set("n", "<leader><leader>", fzf.files)
 vim.keymap.set("n", "<leader>C", fzf.colorschemes)
 vim.keymap.set("n", "<leader>/", fzf.live_grep)
 
+local minifles = require("mini.files")
+vim.keymap.set("n", "<leader>e", minifles.open)
+-- vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open()<CR>", opts)
 
-vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open()<CR>", opts)
 vim.keymap.set("n", "U", "<C-r>", opts)
 vim.keymap.set("n", "L", ":bnext<CR>", opts)
 vim.keymap.set("n", "H", ":bprev<CR>", opts)
